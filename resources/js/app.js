19607dd423065a951a9a5e2b9ff83079101585c0
Ferhnir <zax1984@gmail.com>
2019-09-28 21:24:50 +0100

    Updated project init for vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 require('./bootstrap');
+window.Vue = require('vue');
+
+import Vuetify from 'vuetify';
+Vue.use(Vuetify)
+
+const app = new Vue({
+    vuetify: new Vuetify(),
+    el: '#app'
+});
---
+++ resources/views/index.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+
+        <!-- Styles -->
+        <link href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
+        
+    </head>
+    <body>
+        <v-app id="app">
+            <v-btn primary>Material Button</v-btn>
+        </v-app>
+        <script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.auto.js"></script>
+        <script type="text/javascript" src="js/app.js"></script>
+    </body>
+</html>
